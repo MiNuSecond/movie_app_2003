7e02dae431fb4a132fd58e5cf3e98d5fb850483e
root <mwson987@naver.com>
2020-03-15 16:41:13 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,75 +1,39 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const foodILike = [
-  {
-    id:1,
-    name : "NokduJeon",
-    image :
-    "http://aeriskitchen.com/wp-content/uploads/2018/03/garlic_chive_jeon_01-1-1.jpg",
-    rating : 4.3
-  },
-  {
-    id:2,
-    name : "TteokBokkI",
-    image :
-    "http://aeriskitchen.com/wp-content/uploads/2018/09/spicy_tteokbokki_01-1-1.jpg",
-    rating : 4.5
-  },
-  {
-    id:3,
-    name : "OnionRings",
-    image :
-    "http://aeriskitchen.com/wp-content/uploads/2016/04/onion_rings_01-.jpg",
-    rating : 3.3
-  },
-  {
-    id:4,
-    name : "KoreanEggBread",
-    image :
-    "http://aeriskitchen.com/wp-content/uploads/2012/03/egg_bread_01-.jpg",
-    rating : 4.8
-  },
-  {
-    id:5,
-    name : "KimchiJeon",
-    image :
-    "http://aeriskitchen.com/wp-content/uploads/2010/02/kimchi_jeon_01-1.jpg",
-    rating : 4.0
+class App extends React.Component{
+  
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  };
+  
+  componentDidMount() {
+    console.log("Component rendered");
   }
-  ];
-
-function Food({ fav, picture, rating }) {
-  return (
+  
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  
+  componentWillUnmount() {
+    console.log("Goodbye");
+  }
+  
+  render() {
+    console.log("Im rendering");
+    return (
     <div>
-    <h1>I like {fav}</h1>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={fav} />
+    <h1>The number is:{this.state.count}</h1>
+    <button onClick={this.add}>Add</button>
+    <button onClick={this.minus}>MInux</button>
     </div>
-  );
-}
-
-Food.propTypes = {
-  fav: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating:PropTypes.number
-}
-
-function App() {
-  return (
-  <div>
-    {
-      foodILike.map(
-        dish => 
-          <Food 
-          key ={dish.id} 
-          fav={dish.name}
-          picture={dish.image}
-          rating = {dish.rating} />
-      )
-    }
-  </div>
-  );
+    );
+  }
 }
 
 
